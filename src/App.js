89ed9b46d7b4c8f8aa1d763164389f3209a60efe
qinpengcheng <754530433@qq.com>
2019-05-19 17:20:52 +0800

    新建cnode

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>6666666666</p>
+      </div>
+    );
+  }
+}
+
+export default App;
